f7144685fd4b39be59dd0c58e5d06b4ba935dc13
awilki01 <adam.wilkins@mygrande.net>
2014-10-14 23:10:50 -0500

    First commit

---
+++ desktop/src/com/neet/blockbunny/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.neet.blockbunny.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.neet.blockbunny.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
